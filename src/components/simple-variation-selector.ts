bb3bd6d3ed77d272735424608c15e5eb685ad46c
Eyald <hisco@googlegroups.com>
2019-07-15 09:25:45 +0300

    started varation selector

--- src/components/simple-variation-selector.ts
+++ src/components/simple-variation-selector.ts
@@ -1,8 +1,16 @@
+import { StubzPluginContainer } from './stubz-plugin-container';
 
-export interface StubzVariationSelector{
-
+export interface StubzVariationSelector {
+    plugins:StubzPluginContainer;
 }
 
 export class StubzSimpleVariationSelector{
-
+    plugins:StubzPluginContainer;
+    constructor({
+        plugins
+    }:{
+        plugins:StubzPluginContainer;
+    }){
+        this.plugins = plugins;
+    }
 }
